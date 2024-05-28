b52e051010373afdecdc698ae5be4c84b5e48d31
SyntaxError <116087431+rustamrah@users.noreply.github.com>
2024-05-28 21:17:19 +0500

    Добавьте файлы проекта.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите строку");
+            string line = Console.ReadLine().ToLower();
+            bool isPositive = true;
+            List<string> asciiArray = new List<string>();
+            for (int i = 0; i < 256; i++)
+            {
+                if (i >= 0 && i <= 47 || i >= 58 && i <= 64 ||  i >= 91 && i <= 96 || i >= 123 && i <= 191)
+                {
+                    char c = (char)i;
+                    asciiArray.Add(Convert.ToString(c));
+                }
+            }
+
+            for (int i = 0; i < asciiArray.Count; i++)
+            {
+                line = line.Replace(asciiArray[i], "");
+            }
+            string lineCopy = Reverse(line);
+
+            if (line != "")
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] != lineCopy[i])
+                    {
+                        isPositive = false;
+                    }
+                }
+            }
+            Console.WriteLine(isPositive);
+        }
+    }
+
+    public static string Reverse(string s)
+    {
+        char[] charArray = s.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
